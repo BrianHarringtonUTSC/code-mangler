2c19f4becfd04ce9cb462f3b769749dd18a4f425
Salinder Sidhu <salinder.sid@gmail.com>
2016-01-18 01:32:27 -0500

    Added mongodb connection to server

--- server.py
+++ server.py
@@ -1,5 +1,11 @@
 from flask import Flask
+from flask.ext.mongoengine import MongoEngine
 app = Flask(__name__,static_url_path='')
+
+# Setup connection with MongoDB
+dbSettings = {'db':'codemangler', 'host':'localhost', 'port':27017}
+app.config["MONGODB_SETTINGS"] = dbSettings
+db = MongoEngine(app)
 
 @app.route('/')
 def root():

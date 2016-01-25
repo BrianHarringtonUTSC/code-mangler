1cccfded4e8a9cadeeeb612254bca1c40db2c515
Salinder Sidhu <salinder.sid@gmail.com>
2016-01-24 22:17:06 -0500

    Added sample routes for and model file for mongoDB

--- server.py
+++ server.py
@@ -1,15 +1,24 @@
 from flask import Flask
 from flask.ext.mongoengine import MongoEngine
-app = Flask(__name__,static_url_path='')
 
+# Initialize Flask
+app = Flask(__name__, static_url_path='')
 # Setup connection with MongoDB
-dbSettings = {'db':'codemangler', 'host':'localhost', 'port':27017}
-app.config["MONGODB_SETTINGS"] = dbSettings
+app.config["MONGODB_SETTINGS"] = {"DB": "localhost:27017"}
 db = MongoEngine(app)
 
 @app.route('/')
 def root():
 	return app.send_static_file('index.html')
+
+@app.route('/question', methods = ['GET', 'POST', 'DELETE'])
+def api_question():
+	if request.method == 'GET':
+		return 'GET\n';
+	elif request.method == 'POST':
+		return 'POST\n'
+	elif request.method == 'DELETE':
+		return 'DELETE\n'
 
 if __name__ == '__main__':
     app.run(port=8000)

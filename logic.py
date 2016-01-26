9e38a5bcf12361bf0baba85bbb0851b9d0016345
Umair Idris <umair.idris@mail.utoronto.ca>
2016-01-26 11:47:30 -0500

    Cleanup: Remove backbone, move tmpl to server dir, fix code style in python files, organize html file, add gitignore and remove all unnecessary files.

--- logic.py
+++ logic.py
@@ -1,5 +1,6 @@
 def isFieldsExist(data, list_fields):
+    """Returns true if list_fields are all fields in data, else False."""
     for field in list_fields:
-        if field not in data:
+      if field not in data:
             return False
     return True
--- server.py
+++ server.py
@@ -1,23 +1,18 @@
-from flask import Flask, jsonify, request, abort
+from flask import Flask, jsonify, request, abort, render_template
 from mongoengine import connect
-from models import *
-from logic import *
+
+import models
+import logic
 import os
 
-# Initialize Flask
-app = Flask(__name__, static_url_path='')
+app = Flask(__name__, static_url_path='', template_folder='tmpl/')
 
-# Setup MongoDB connection
 connect('codemangler', host='localhost', port=27017)
 
 
 @app.route('/')
 def index():
-    return app.send_static_file('index.html')
-
-@app.route('/main')
-def mainPage():
-    return app.send_static_file('tmpl/Home.html')
+    return render_template('Home.html')
 
 @app.route('/login', methods=['GET'])
 def login():
@@ -29,22 +24,18 @@ def get_question():
     # For now the return data is hardcoded
     temp = [{'topic': 'For Loops'},
             {'lines': ['for w in words', 'print(w)']}]
-    # Jsonify and return the data
     response = jsonify(result=temp)
     return response
 
 
 @app.route('/question', methods=['POST'])
 def post_question():
-    # Parse data and define fields to check
     fields = ['topic', 'lines']
     parsed_data = request.get_json(silent=True)
-    # Check if request data is valid
-    if isFieldsExist(parsed_data, fields):
-        # Obtain the topic and code lines from the parsed data
+
+    if logic.isFieldsExist(parsed_data, fields):
         question_topic = parsed_data.get('topic')
         code_lines = parsed_data.get('lines').split(',')
-        # Store as a new document in the database and return id of new entry
         new_question = Question(topic=question_topic, lines=code_lines).save()
         return jsonify(result=str(new_question.id))
     else:

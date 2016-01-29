f5e9c2778985bac93ac87d8345522eb70e0e2005
Umair Idris <umair.idris@mail.utoronto.ca>
2016-01-28 23:29:48 -0500

    Add questions page and routes. Cleanup code and update css.

--- server.py
+++ server.py
@@ -12,12 +12,12 @@ Question = namedtuple('Question', 'id question solution scramble_order')
 answer_regex = re.compile('^((\s*[0-9]+),)+(\s*[0-9]+)$')
 
 # hardcoded data for now. TODO: add DB
-data = Question(1, 'Calculate sum of a list of numbers.',
+data = Question(1, 'Calculate the sum of a list of numbers.',
         ['def sum(L):', '  sum = 0', '  for item in L:', '    sum += item', '  return sum'], [4, 1, 2, 3, 0])
 
 @app.route('/')
-def index():
-    return render_template('drag.html')
+def get_questions():
+    return render_template('questions.html', questions=[data])
 
 @app.route('/question/<question_id>', methods=['GET'])
 def get_question(question_id):
@@ -28,10 +28,13 @@ def get_question(question_id):
 def answer_question(question_id):
     req_ans = request.form['answer']
     if not answer_regex.match(req_ans):
-        return 'Wrong'
+        return ('Invalid', 400)
 
-    ans = [int(i) for i in req_ans.split(',')]
-    return 'Correct' if ans == data.scramble_order else 'Wrong'
+    scramble_order = [int(i) for i in req_ans.split(',')]
+    return 'Correct' if scramble_order == data.scramble_order else 'Wrong'
+
+def drag():
+    return render_template('drag.html')
 
 
 if __name__ == '__main__':

600f2228d29998f57d1d3429bc8bab3d68709c50
TanjidIslam <tanjid.islam@mail.utoronto.ca>
2016-02-08 04:32:12 -0500

    Major Changes - Removed hardcode and added MongoDB on localhost - Fixed front-end to be compatible with MongoDB

--- server.py
+++ server.py
@@ -1,5 +1,6 @@
 from collections import namedtuple
 from flask import Flask, jsonify, request, abort, render_template
+from pymongo import MongoClient
 
 import os
 import json
@@ -7,30 +8,47 @@ import re
 
 app = Flask(__name__, static_url_path='', template_folder='tmpl/')
 
-Question = namedtuple('Question', 'id question solution scramble_order')
 
-answer_regex = re.compile('^((\s*[0-9]+),)*(\s*[0-9]+)$')
-
-# hardcoded data for now. TODO: add DB
-data = Question(1, 'Calculate the sum of a list of numbers.',
-                ['def sum(L):', '  sum = 0', '  for item in L:', '    sum += item', '  return sum'],
-                [4, 1, 2, 3, 0])
+def get_collection(db_name, collections):
+    # Setting up MongoDb Connection
+    client = MongoClient('localhost', 27017)
+    db = client[db_name]
+    all = db[collections]
+    question = all.find()
+    data = []
+    for item in question:
+        data.append(item)
+    return data
 
 @app.route('/')
 def get_questions():
-    return render_template('questions.html', questions=[data])
+    data = get_collection("question_db", "questions")
+    return render_template('questions.html', questions=data)
 
 @app.route('/question/<question_id>', methods=['GET'])
 def get_question(question_id):
-    lines = [data.solution[i].lstrip() for i in data.scramble_order]
-    return render_template('question.html', id=data.id, question=data.question, lines=lines)
+    data = get_collection("question_db", "questions")[int(question_id)-1]
+    solution = data['solution']
+    scramble_order = data['scramble_order']
+    return render_template(
+        'question.html',
+        id=data['id'],
+        question=data['question'],
+        description=data['description'],
+        lines=[solution[i].lstrip() for i in scramble_order]
+    )
 
 @app.route('/question/<question_id>', methods=['POST'])
 def answer_question(question_id):
+
     ans = json.loads(request.form.get('answer', '[]'))
-    return 'Correct' if ans == data.scramble_order else 'Try Again'
+    data = get_collection("question_db", "questions")[int(question_id)-1]
+    return 'Correct' if ans == data['scramble_order'] else 'Try Again'
 
 
 if __name__ == '__main__':
     port = int(os.getenv('PORT', 8000))
+
+    # Setting up local server for Flask
+    app.debug = True
     app.run(port=port)

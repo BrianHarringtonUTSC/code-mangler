849ae96862e107cfc5ba0e1a65d257829277ed96
TanjidIslam <tanjid.islam@mail.utoronto.ca>
2016-02-08 12:54:39 -0500

    MongoDB is now on remote server

--- server.py
+++ server.py
@@ -1,15 +1,27 @@
-from collections import namedtuple
-from flask import Flask, jsonify, request, abort, render_template
+from flask import Flask, request, render_template
 from pymongo import MongoClient
 
 import os
 import json
-import re
 
 app = Flask(__name__, static_url_path='', template_folder='tmpl/')
 
-
 def get_collection(db_name, collections):
+    server = 'ds059375.mongolab.com'
+    port = 59375
+    username = 'tanjid'
+    password = 'pwd123'
+    client = MongoClient(server, port)
+    db = client[db_name]
+    db.authenticate(username, password, mechanism='SCRAM-SHA-1')
+    posts = db[collections]
+    question = posts.find()
+    data = []
+    for item in question:
+        data.append(item)
+    return data
+
+def get_collection_local(db_name, collections):
     # Setting up MongoDb Connection
     client = MongoClient('localhost', 27017)
     db = client[db_name]
@@ -22,12 +34,14 @@ def get_collection(db_name, collections):
 
 @app.route('/')
 def get_questions():
-    data = get_collection("question_db", "questions")
+    data = get_collection("code_mangler", "questions")
+    #data = get_collection_local("question_db", "questions")
     return render_template('questions.html', questions=data)
 
 @app.route('/question/<question_id>', methods=['GET'])
 def get_question(question_id):
-    data = get_collection("question_db", "questions")[int(question_id)-1]
+    data = get_collection("code_mangler", "questions")[int(question_id)-1]
+    #data = get_collection_local("question_db", "questions")
     solution = data['solution']
     scramble_order = data['scramble_order']
     return render_template(
@@ -42,7 +56,7 @@ def get_question(question_id):
 def answer_question(question_id):
 
     ans = json.loads(request.form.get('answer', '[]'))
-    data = get_collection("question_db", "questions")[int(question_id)-1]
+    data = get_collection("code_mangler", "questions")[int(question_id)-1]
     return 'Correct' if ans == data['scramble_order'] else 'Try Again'
 
 

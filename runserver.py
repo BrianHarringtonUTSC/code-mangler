20ce9046762bd26471abfef010c0879111bd5d61
TanjidIslam <tanjid.islam@mail.utoronto.ca>
2016-04-14 12:54:15 -0400

    changed host

--- runserver.py
+++ runserver.py
@@ -2,4 +2,6 @@ import os
 
 from codemangler import app
 
-app.run(port=int(os.getenv('PORT', 8000)), debug=True, threaded=True)
+# app.run(port=int(os.getenv('PORT', 8000)), debug=True, threaded=True)
+app.run(host=os.getenv("IP", "142.1.97.144"),port=int(os.getenv("PORT", 5000)))
+

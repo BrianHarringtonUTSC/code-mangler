0c10d1c09df49a758427a12b713eeea631d19889
TanjidIslam <tanjid.islam@live.ca>
2016-04-18 15:34:05 -0400

    Update runserver.py

--- runserver.py
+++ runserver.py
@@ -3,5 +3,5 @@ import os
 from codemangler import app
 
 # app.run(port=int(os.getenv('PORT', 8000)), debug=True, threaded=True)
-app.run(host=os.getenv("IP", "142.1.97.144"),port=int(os.getenv("PORT", 5000)))
+app.run(host=os.getenv("IP", "142.1.97.144"),port=int(os.getenv("PORT", 15003)))
 
